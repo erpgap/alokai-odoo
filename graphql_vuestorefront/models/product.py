3c356458fd67257faea638550e7c3b0fda2cf9b7
Ajay Javiya <ajaviya@odoogap.com>
2024-11-13 10:27:48 +0530

    [IMP] avoid create multiple sale order line for same product

--- graphql_vuestorefront/models/product.py
+++ graphql_vuestorefront/models/product.py
@@ -287,6 +287,13 @@ class ProductTemplate(models.Model):
     def recalculate_products_popularity(self):
         self.search([])._compute_sales_count_30_days()
 
+    def _has_no_variant_attributes(self):
+        """ Overwrite : always return False regardless of product attributes variant creation mode setting
+        to avoid create multiple sale order line for same product
+        """
+        self.ensure_one()
+        return False
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
