e92e4b10b253bb157d293c50e700a8562d061e83
Carlos Fonseca <cfonseca@odoogap.com>
2024-11-15 14:29:58 +0000

    [ADD] slug filter to blog post query and search filter to blog posts query

--- graphql_vuestorefront/schemas/website_blog.py
+++ graphql_vuestorefront/schemas/website_blog.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 import graphene
-from graphql import GraphQLError
-
-from odoo.http import request
 from odoo import _
 from odoo.addons.graphql_vuestorefront.schemas.objects import (
     BlogPost,
@@ -10,6 +7,7 @@ from odoo.addons.graphql_vuestorefront.schemas.objects import (
     get_document_with_check_access,
     get_document_count_with_check_access
 )
+from odoo.osv import expression
 
 def get_search_order(sort):
     sorting = ''
@@ -52,34 +50,46 @@ class BlogPostQuery(graphene.ObjectType):
         BlogPost,
         required=True,
         id=graphene.Int(),
+        slug=graphene.String(default_value=None),
     )
     blog_posts = graphene.Field(
         BlogPosts,
         filter=graphene.Argument(BlogPostFilterInput, default_value={}),
         current_page=graphene.Int(default_value=1),
         page_size=graphene.Int(default_value=10),
+        search=graphene.String(default_value=False),
         sort=graphene.Argument(BlogPostSortInput, default_value={})
     )
 
     @staticmethod
-    def resolve_blog_post(self, info, id):
+    def resolve_blog_post(self, info, id=None, slug=None):
         BlogPost = info.context['env']['blog.post']
-        error_msg = 'Blog Post does not exist.'
-        blog_post = get_document_with_check_access(BlogPost, [('id', '=', id)], error_msg=error_msg)
-        if not blog_post:
-            raise GraphQLError(_(error_msg))
+
+        if id:
+            domain = [('id', '=', id)]
+            blog_post = get_document_with_check_access(BlogPost, domain)
+        elif slug:
+            domain = [('website_slug', '=', slug)]
+            blog_post = get_document_with_check_access(BlogPost, domain)
+        else:
+            blog_post = BlogPost
+
         return blog_post.sudo()
 
     @staticmethod
-    def resolve_blog_posts(self, info, filter, current_page, page_size, sort):
+    def resolve_blog_posts(self, info, filter, current_page, page_size, search, sort):
         env = info.context["env"]
-        user = request.env.user
         sort_order = get_search_order(sort)
         domain = []
 
+        if search:
+            for srch in search.split(" "):
+                domain.append([
+                    '|', ('name', 'ilike', srch), ('content', 'like', srch)])
+
         # Filter by stages or default to sales and done
         if filter.get('tag_id', False):
-            domain += [('tag_ids', 'in', filter['tag_id'])]
+            domain.append([('tag_ids', 'in', filter['tag_id'])])
 
         # First offset is 0 but first page is 1
         if current_page > 1:
@@ -88,7 +98,7 @@ class BlogPostQuery(graphene.ObjectType):
             offset = 0
 
         BlogPost = env['blog.post']
-        blog_posts = get_document_with_check_access(BlogPost, domain, sort_order, page_size, offset,
+        blog_posts = get_document_with_check_access(BlogPost, expression.AND(domain), sort_order, page_size, offset,
                                                 error_msg='Blog Post  does not exist.')
-        total_count = get_document_count_with_check_access(BlogPost, domain)
+        total_count = get_document_count_with_check_access(BlogPost, expression.AND(domain))
         return BlogPostList(blog_posts=blog_posts and blog_posts.sudo() or blog_posts, total_count=total_count)
