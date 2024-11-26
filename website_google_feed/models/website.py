bdb71fe00e822a480eab129c15b7aa3d5ed6214a
Ajay Javiya <ajaviya@odoogap.com>
2024-11-26 13:43:00 +0530

    [MIG] website_google_feed : module to version 18

--- website_google_feed/models/website.py
+++ website_google_feed/models/website.py
@@ -2,7 +2,7 @@
 # Copyright 2024 ERPGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, fields, models, _
+from odoo import fields, models, _
 
 
 class Website(models.Model):
