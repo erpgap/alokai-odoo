29535c6e2bfd12d717bf66693c31fc45c40fa626
Carlos Fonseca <cfonseca@odoogap.com>
2024-11-29 12:06:04 +0000

    [IMP] order by publish date desc blog posts by default

--- graphql_alokai/schemas/website_blog.py
+++ graphql_alokai/schemas/website_blog.py
@@ -9,6 +9,7 @@ from odoo.addons.graphql_alokai.schemas.objects import (
 )
 from odoo.osv import expression
 
+
 def get_search_order(sort):
     sorting = ''
     for field, val in sort.items():
@@ -18,9 +19,9 @@ def get_search_order(sort):
 
     # Add id as last factor, so we can consistently get the same results
     if sorting:
-        sorting += ', id ASC'
+        sorting += ', published_date DESC, id ASC'
     else:
-        sorting = 'id ASC'
+        sorting = 'published_date DESC, id ASC'
 
     return sorting
 
