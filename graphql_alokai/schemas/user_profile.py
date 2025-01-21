1197a95ab7ad85088c9c80f5585b2980232f84c6
Ajay Javiya <ajaviya@odoogap.com>
2025-01-21 17:34:45 +0530

    [ADD] mutation to delete user profile/account

--- graphql_alokai/schemas/user_profile.py
+++ graphql_alokai/schemas/user_profile.py
@@ -4,7 +4,7 @@
 
 import graphene
 from graphql import GraphQLError
-from odoo import _
+from odoo import _, SUPERUSER_ID
 from odoo.http import request
 
 from odoo.addons.graphql_alokai.schemas.objects import Partner
@@ -61,5 +61,34 @@ class UpdateMyAccount(graphene.Mutation):
         return partner
 
 
+class DeleteMyAccount(graphene.Mutation):
+
+    Output = graphene.Boolean
+
+    @staticmethod
+    def mutate(self, info):
+        env = info.context["env"]
+        website = env['website'].get_current_website()
+        request.website = website
+        user = request.env.user.sudo()
+        website_user = website.user_id
+
+        # Prevent "Public User" to be deleted
+        if user.id == website_user.id or not user.has_group('base.group_portal'):
+            raise GraphQLError(_('User cannot be deleted.'))
+
+        if user:
+            request.session.logout()
+            if not user.email:
+                raise GraphQLError(_(f"Cannot send email: user {user.name} has no email address."))
+            user._deactivate_portal_user()
+            template = env.ref('graphql_alokai.delete_my_account_email').sudo()
+            template.send_mail(user.id, raise_exception=True, force_send=True)
+        else:
+            raise GraphQLError(_('User does not exist.'))
+        return True
+
+
 class UserProfileMutation(graphene.ObjectType):
     update_my_account = UpdateMyAccount.Field(description='Update MyAccount')
+    delete_my_account = DeleteMyAccount.Field(description='Delete MyAccount')
