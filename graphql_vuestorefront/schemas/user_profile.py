3bd7a54724a8196b512862494cb0132bc757edc7
Ajay Javiya <ajaviya@odoogap.com>
2025-02-26 18:38:55 +0530

    [IMP] UpdateMyAccount: password is only required if email change

--- graphql_vuestorefront/schemas/user_profile.py
+++ graphql_vuestorefront/schemas/user_profile.py
@@ -39,12 +39,12 @@ class UpdateMyAccountParams(graphene.InputObjectType):
 class UpdateMyAccount(graphene.Mutation):
     class Arguments:
         myaccount = UpdateMyAccountParams()
-        password = graphene.String(required=True)
+        current_password = graphene.String()
 
     Output = Partner
 
     @staticmethod
-    def mutate(self, info, myaccount, password):
+    def mutate(self, info, myaccount, current_password=''):
         env = info.context["env"]
         website = env['website'].get_current_website()
         user = request.env.user
@@ -57,18 +57,24 @@ class UpdateMyAccount(graphene.Mutation):
         partner = user.partner_id
         if not partner:
             raise GraphQLError(_('Partner does not exist.'))
-        # try:
-        user._check_credentials(password, env)
-        partner.write(myaccount)
-        if myaccount.get('email'):
-            user.login = myaccount['email']
-        env.cr.commit()
-        request.session.authenticate(request.session.db, user.login, password)
-        if bool(user._mfa_type()):
-            request.session.finalize(request.env)
-        return partner
-        # except Exception as e:
-        #     raise GraphQLError(_('Incorrect password.'))
+        if myaccount.get('email') and partner.email != myaccount['email']:
+            if not current_password:
+                raise GraphQLError(_('Password is required to change email.'))
+            try:
+                partner.write(myaccount)
+                user._check_credentials(current_password, env)
+                if myaccount.get('email'):
+                    user.login = myaccount['email']
+                env.cr.commit()
+                request.session.authenticate(request.session.db, user.login, current_password)
+                if bool(user._mfa_type()):
+                    request.session.finalize(request.env)
+                return partner
+            except Exception as e:
+                raise GraphQLError(_('Incorrect password.'))
+        else:
+            partner.write(myaccount)
+            return partner
 
 class UserProfileMutation(graphene.ObjectType):
     update_my_account = UpdateMyAccount.Field(description='Update MyAccount')
