81547ac1f1237e29719f84ba78069ad730a89276
Carlos Fonseca <cfonseca@odoogap.com>
2025-03-03 09:27:35 +0000

    [FIX] elevate privileges to be able to fetch wishlist of logged in user

--- graphql_vuestorefront/schemas/sign.py
+++ graphql_vuestorefront/schemas/sign.py
@@ -63,7 +63,7 @@ class Login(graphene.Mutation):
             if website.vsf_mailing_list_id and subscribe_newsletter:
                 MassMailController().subscribe(website.vsf_mailing_list_id.id, email, 'email')
 
-            wishlist_items = env['product.wishlist'].search([
+            wishlist_items = env['product.wishlist'].sudo().search([
                 ('partner_id', '=', user.partner_id.id), ('website_id', '=', website.id)])
             wishlist_items = wishlist_items.filtered(
                 lambda wish:
