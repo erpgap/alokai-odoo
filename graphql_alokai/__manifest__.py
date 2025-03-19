6afe6edebf9cb54b0a5cd1429b9ce03151246968
Ajay Javiya <ajaviya@odoogap.com>
2025-03-19 09:34:01 +0530

    [ADD] company_reg_no on partner

--- graphql_alokai/__manifest__.py
+++ graphql_alokai/__manifest__.py
@@ -38,6 +38,7 @@
         'views/alokai_website_page_views.xml',
         'views/website_blog_views.xml',
         'views/res_config_settings_views.xml',
+        'views/res_partner_views.xml',
         'views/menu.xml'
     ],
     'demo': [
