c329c4edec96c14247890b2e416f587a7dc46900
Ajay Javiya <ajaviya@odoogap.com>
2025-03-19 14:47:54 +0530

    Revert "[ADD] company_reg_no on partner"
    This reverts commit 6afe6edebf9cb54b0a5cd1429b9ce03151246968.

--- graphql_alokai/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    company_reg_no = fields.Char('Company Registration Number')
