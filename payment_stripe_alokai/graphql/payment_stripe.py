f62df978ef51bf41fdfb2628011cfc0e9c03d2f3
Diogo Duarte <dduarte@erpgap.com>
2025-03-27 10:25:06 +0000

    [IMP] decouple payment_stripe_alokai

--- payment_stripe_alokai/graphql/payment_stripe.py
+++ payment_stripe_alokai/graphql/payment_stripe.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2024 ERPGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# -*- coding: utf-8 -*-
+# Copyright 2024 ERPGAP/PROMPTEQUATION LDA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 import graphene
 from graphene.types import generic
