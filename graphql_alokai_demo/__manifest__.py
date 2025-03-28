ab585308b358384d594b7e1523c3a229ff8298ce
Diogo Duarte <dduarte@erpgap.com>
2025-03-28 19:11:59 +0000

    [IMP] add module: graphql_alokai_demo

---
+++ graphql_alokai_demo/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 ERPGAP/PROMPTEQUATION LDA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "GraphQL Alokai Demo",
+    'description': """
+Demo Alokai extension module for Odoo
+=====================================
+The purpose of this module is to demonstrate the how to use the Alokai GraphQL API in Odoo.
+The example is for demo only and not for production use.
+Check graphql/product.py for the example of how to extend an existing type/model.
+    """,
+    "version": "18.0.1.0.1",
+    'license': 'LGPL-3',
+    'author': 'ERPGAP',
+    'website': 'https://www.erpgap.com/',
+    "depends": ["graphql_alokai"],
+    "development_status": "Beta",
+    "installable": True,
+}
