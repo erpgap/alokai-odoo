0f791a36cf5d4007af804fb17a525defdb5aade0
Diogo Duarte <dduarte@erpgap.com>
2025-04-01 16:26:40 +0100

    [FIX] cammel case to snake case

--- payment_adyen_alokai/graphql/__init__.py
+++ payment_adyen_alokai/graphql/__init__.py
@@ -1,1 +1,5 @@
-from . import adyenPayment
+# -*- coding: utf-8 -*-
+# Copyright 2024 ERPGAP/PROMPTEQUATION LDA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import payment_adyen
--- payment_adyen_alokai/graphql/payment_adyen.py
+++ payment_adyen_alokai/graphql/payment_adyen.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 ERPGAP/PROMPTEQUATION LDA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
 import graphene
 from graphene.types import generic
 from graphql import GraphQLError
