8593285fcb5f85f532bf45e1897ab2e13e6948c7
Carlos Fonseca <cfonseca@odoogap.com>
2025-06-13 12:10:07 +0100

    [FIX] trigger compute attribute values for products with attributes without variants

--- graphql_vuestorefront/models/product.py
+++ graphql_vuestorefront/models/product.py
@@ -210,7 +210,7 @@ class ProductTemplate(models.Model):
                     slug_name = slugify(product.name or '').strip().strip('-')
                     product.website_slug = f'{prefix}/{slug_name}-{product.id}'
 
-    @api.depends('product_variant_ids')
+    @api.depends('product_variant_ids', 'product_variant_id')
     def _compute_variant_attribute_value_ids(self):
         """
         Used to filter attribute values on the website.
