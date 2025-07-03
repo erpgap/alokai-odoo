071c7a0f32656adf5618bb840b618c6ffa50ee20
Ajay Javiya <ajaviya@odoogap.com>
2025-07-03 17:48:15 +0530

    [IMP] on create or update sale order line reset carrier and delivery line

--- graphql_vuestorefront/schemas/shop.py
+++ graphql_vuestorefront/schemas/shop.py
@@ -116,6 +116,9 @@ class CartAddMultipleItems(graphene.Mutation):
             product_id = product['id']
             quantity = product['quantity']
             order._cart_update(product_id=product_id, add_qty=quantity)
+        # reset carrier
+        order.carrier_id = False
+        order._remove_delivery_line()
         return CartData(order=order)
 
 
@@ -137,6 +140,9 @@ class CartUpdateMultipleItems(graphene.Mutation):
             # Reset Warning Stock Message always before a new update
             line.shop_warning = ""
             order._cart_update(product_id=line.product_id.id, line_id=line.id, set_qty=quantity)
+        # reset carrier
+        order.carrier_id = False
+        order._remove_delivery_line()
         return CartData(order=order)
 
 
@@ -154,6 +160,9 @@ class CartRemoveMultipleItems(graphene.Mutation):
         for line_id in line_ids:
             line = order.order_line.filtered(lambda rec: rec.id == line_id)
             line.unlink()
+        # reset carrier
+        order.carrier_id = False
+        order._remove_delivery_line()
         return CartData(order=order)
 
 
